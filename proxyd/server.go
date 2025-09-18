bffd952cc073c2baa32d4874f4e097241e243dc2
Vui-Chee <46051576+Vui-Chee@users.noreply.github.com>
2025-09-18 16:06:49 +0800

    ws: route requests from k8s ingress with custom path (#12)
    * WS route correction for k8s ingress path
    
    * Accept "/*" for incoming WS requests.
    
    * Handle 2 or more segments.
    
    * Drop comment
    
    * Add basic validation.

--- proxyd/server.go
+++ proxyd/server.go
@@ -239,6 +239,7 @@ func (s *Server) WSListenAndServe(host string, port int) error {
 	hdlr := mux.NewRouter()
 	hdlr.HandleFunc("/", s.HandleWS)
 	hdlr.HandleFunc("/{authorization}", s.HandleWS)
+	hdlr.HandleFunc("/{path:.+/.+}", s.HandleManySegsWS) // 2 or more segments
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"},
 	})
